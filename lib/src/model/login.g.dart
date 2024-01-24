fe04a8754cae9918ee000544127c2248d0bbd7a0
sootsun <87323981+sootsun@users.noreply.github.com>
2024-01-24 21:48:26 +0900

    /member (1.24)

---
+++ lib/src/model/login.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Login _$LoginFromJson(Map<String, dynamic> json) => Login(
+      name: json['name'] as String,
+      password: json['password'] as String,
+    );
+
+Map<String, dynamic> _$LoginToJson(Login instance) => <String, dynamic>{
+      'name': instance.name,
+      'password': instance.password,
+    };
